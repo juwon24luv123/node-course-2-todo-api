6df12bfad5ad04fa05c0af4ed77ca833803ee80e
juwon24luv123 <juwon24luv1@yahoo.com>
2018-01-12 18:11:40 -0800

    add post/ todos route

---
+++ server/server.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var bodyParper = require('body-parser');
+
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParper.json());
+
+app.post('/todos', (req, res) =>{
+    var todo = new Todo({
+        text:req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000,() => {
+    console.log('Started on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var User = new User ({
+//     email: '    juwon24luv1@gmail.com     '
+// });
+
+// User.save().then((doc) =>{
+//     console.log('User saved', doc);
+// }, (e) =>{
+//     console.log('Unable to save user', e);
+// });
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo')
+// });
+
+
+// var classTodo = new Todo({
+//     text: '   edit this video   '
+//     // text: 'mongodb is interesting',
+//     // completed: true,
+//     // completedAt: 123
+// });
+
+// classTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save todo', e);
+// });
+
+
