81e86ba5412c90249d01302f40766923e50249f7
juwon24luv123 <juwon24luv1@yahoo.com>
2018-01-15 14:33:07 -0800

    Add Get /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var bodyParper = require('body-parser');
+var {ObjectID} = require('mongodb');
+
 
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 
@@ -35,6 +38,26 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// GET / TODO/12345
+
+app.get('/todos/:id', (req, res) => {
+    // res.send(req.params);
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
